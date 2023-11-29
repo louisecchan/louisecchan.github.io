f757dd607a4ff1bab21c58d0fbc7985dbe680def
Louise <louise.cch@gmail.com>
2023-11-29 22:49:05 +0800

    mbr

--- files/hamburger.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementsByClassName("nav-menu")[0];
-  if (x.className === "nav-menu") {
-    x.className += "responsive";
-  } else {
-    x.className = "nav-menu";
-  }
-}
